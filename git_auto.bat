a360e41bde11580bb374885d3fa0288b2844bc09
BHANU <jennyguddanti@gmail.com>
2024-09-28 18:34:04 +0200

    Commited with Git Automation Bot!

--- git_auto.bat
+++ git_auto.bat
@@ -2,11 +2,16 @@
 cd /d "C:/Users/BHANU/Downloads/coderstarrer.github.io-main"
 
 
+
+REM Pull latest changes
+git pull origin main
+
 REM Add all changes
-git add . || exit /b
+git add .
 
 REM Commit changes with a message
-git commit -m "Git Commit Change Automation with Git AUTO Bot" || exit /b
+git commit -m "Commited with Git Automation Bot!"
 
 REM Push changes to remote
-git push origin main || exit /b
+git push origin main
+
