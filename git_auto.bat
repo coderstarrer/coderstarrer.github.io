5b0713977f3002e917956ca4715af67ee367c241
BHANU <jennyguddanti@gmail.com>
2024-09-28 18:17:03 +0200

    Git Commit Change Automation with Git AUTO Bot

--- git_auto.bat
+++ git_auto.bat
@@ -1,15 +1,17 @@
 @echo off
 cd /d "C:/Users/BHANU/Downloads/coderstarrer.github.io-main"
 
+REM Check GIT Status
+git status
+
 REM Pull latest changes
-git pull origin main
+git pull origin main || exit /b
 
 REM Add all changes
-git add .
+git add . || exit /b
 
 REM Commit changes with a message
-git commit -m "Git Commit Change Automation with Git AUTO Bot"
+git commit -m "Git Commit Change Automation with Git AUTO Bot" || exit /b
 
 REM Push changes to remote
-git push origin main
-
+git push origin main || exit /b
