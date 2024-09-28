d7c31b7e3fe1d8d0286aa4ddd5f59c2c6fb43d38
BHANU <jennyguddanti@gmail.com>
2024-09-28 19:18:16 +0200

    Commited with Git Automation Bot!

--- git_auto.bat
+++ git_auto.bat
@@ -1,17 +1,17 @@
 @echo off
 cd /d "C:/Users/BHANU/Downloads/coderstarrer.github.io-main"
 
-
-
 REM Pull latest changes
 git pull origin main
 
 REM Add all changes
 git add .
 
-REM Commit changes with a message
-git commit -m "Commited with Git Automation Bot!"
+REM Prompt for commit message
+set /p commitMessage="What did you commit? "
+
+REM Commit changes with the user-provided message
+git commit -m "%commitMessage%"
 
 REM Push changes to remote
 git push origin main
-
