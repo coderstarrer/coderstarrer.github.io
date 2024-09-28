f52c68bacfddfbd9ffa39ad9bdb8da0318d5e737
V3ZE <136115292+coderstarrer@users.noreply.github.com>
2024-09-28 12:31:22 +0200

    Delete script.ts

--- script.ts
+++
@@ -1,9 +0,0 @@
-document.getElementById("contact-form")?.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const name = (document.getElementById("name") as HTMLInputElement).value;
-    const email = (document.getElementById("email") as HTMLInputElement).value;
-    const message = (document.getElementById("message") as HTMLTextAreaElement).value;
-
-    console.log("Contact Form Submitted:", { name, email, message });
-    alert("Thank you for your message, " + name + "!");
-});
