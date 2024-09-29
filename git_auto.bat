7820840bdf33dc33664ba49246b1525a0b597de5
BHANU <jennyguddanti@gmail.com>
2024-09-29 11:52:52 +0200

    cmd /c c:\Users\BHANU\Downloads\coderstarrer.github.io-main\git_auto.bat

--- git_auto.bat
+++ git_auto.bat
@@ -1,12 +1,16 @@
 @echo off
 cd /d "C:/Users/BHANU/Downloads/coderstarrer.github.io-main"
 
+REM Pull latest changes
+git pull origin main
 
 REM Add all changes
 git add .
 
 REM Prompt for commit message
 set /p commitMessage="Commit message: "
+
+
 
 REM Commit changes with the user-provided message
 git commit -m "%commitMessage%"
