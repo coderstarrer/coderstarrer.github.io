7a8b0320adbdbd734d9eb7a05a4decf42a1038a6
BHANU <jennyguddanti@gmail.com>
2024-10-01 15:57:40 +0200

    Enhanced mobile support once again

--- tempCodeRunnerFile.bat
+++ tempCodeRunnerFile.bat
@@ -1,16 +1,3 @@
-@echo off
-cd /d "C:/Users/BHANU/Downloads/coderstarrer.github.io-main"
 
-REM Add all changes
-git add .
-
-REM Prompt for commit message
-set /p commitMessage="Commit message: "
-
-
-
-REM Commit changes with the user-provided message
-git commit -m "%commitMessage%"
-
-REM Push changes to remote
-git push origin main
+    exit /b 1
+)
